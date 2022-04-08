d17c195d23a9ba2298dfe9ed4a4487d547eb933f
Billy Cole <billycole@macbook-pro.mynetworksettings.com>
2022-04-08 15:05:23 -0400

    Render heading and form. Establish basic file structure.

---
+++ src/App/App.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import TodoForm from '../components/TodoForm/TodoForm.svelte';
+</script>
+
+<main>
+	<h1>Svelte Todo</h1>
+	<TodoForm />
+</main>
+
+<style src="./App.css" scoped></style>
---
+++ src/components/TodoForm/TodoForm.svelte
@@ -0,0 +1,10 @@
+<script lang="ts"></script>
+
+<form>
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname"><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname">
+</form>
+
+<style src="./TodoForm.css" scoped></style>
