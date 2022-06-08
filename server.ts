bcc2c365e08855b17833ad662ef5f6ae54a8764b
Billy Cole <billycole@billys-mbp.mynetworksettings.com>
2022-06-08 14:47:18 -0400

    Add app.listen in server file.

--- server.ts
+++ server.ts
@@ -9,3 +9,7 @@ app.use(express.static("public"));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "public", "index.html"));
 });
+
+app.listen(port, () =>
+  console.log(`Server is running on: http://localhost:${port}`)
+);
